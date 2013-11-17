52cce1b6d48842f36f777a268c69a125cd5605d5
Brandon Edgren <brandon.edgren@gmail.com>
2013-11-16 21:24:57 -0800

    Used new constants and added way to get ranks of scores.

--- source/high_scores_helpers.py
+++ source/high_scores_helpers.py
@@ -1,6 +1,6 @@
 
 import time, sqlite3
-from constants import SCORES_DB
+from constants import HS_ENTRY_MAX_NAME_LENGTH, SCORES_DB
 
 class Score:
     def __init__(self, name, points, level, date):
@@ -8,6 +8,10 @@ class Score:
         self.points = points
         self.level = level
         self.date = date
+
+    def __eq__(self, other):
+        return (self.name == other.name and self.points == other.points and
+                self.level == other.level and self.date == other.date)
 
 #TODO: external database for global high scores (how to securely access?)
 class HighScores:
@@ -23,6 +27,7 @@ class HighScores:
 
     def _setupdb(self):
         self._connectdb()
+        date_size = 24
         # name - the players name
         # points - the players score
         # level - the maximum level achieved by the player
@@ -31,12 +36,12 @@ class HighScores:
         # A primary key does not make sense for this table
         self.cursor.execute('''
             CREATE TABLE scores (
-            name VARCHAR(10),
+            name VARCHAR(%d),
             points int,
             level int,
-            date VARCHAR(24),
+            date VARCHAR(%d),
             synced int
-            )''')
+            )''' % (HS_ENTRY_MAX_NAME_LENGTH, date_size))
 
     def _connectdb(self):
         self.database = sqlite3.connect(SCORES_DB)
@@ -44,6 +49,21 @@ class HighScores:
 
     def __del__(self):
         self.database.close()
+
+    """ Returns the rank of the givien score, or 0 if the score doesn't exist """
+    def getLocalRank(self, score):
+        scores = []
+        rows = self.cursor.execute('''
+            SELECT *
+            FROM scores
+            ORDER BY points DESC''')
+        for row in rows:
+            scores.append(Score(row[0], row[1], row[2], row[3]))
+
+        if score not in scores:
+            return 0
+        
+        return scores.index(score) + 1
 
     """ Gets the top n local high scores """
     def localTop(self, n):
@@ -66,12 +86,18 @@ class HighScores:
         pass
 
     def newScore(self, name, points, level):
-        row = (name, points, level, time.ctime())
+        data = time.ctime()
+        row = (name, points, level, date)
         self.cursor.execute('''
             INSERT INTO scores
             VALUES(?, ?, ?, ?, 0)
         ''', row)
         self.database.commit()
+
+        local_rank = self.getLocalRank(Score(name, points, level, date))
+        global_rank = 0
+        assert local_rank != 0 # We just added it so this would indicate a problem
+        return local_rank, global_rank
 
 # Gobal variable acts as Singleton
 HighScores = HighScores()
